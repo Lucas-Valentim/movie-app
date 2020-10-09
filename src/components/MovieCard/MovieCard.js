cf3b6d942246ed63aebc0ea39b9d4c97faf0a4d3
Lucas Valentim <lucasv.27@hotmail.com>
2020-10-08 22:50:53 -0300

    Initial commit

---
+++ src/components/MovieCard/MovieCard.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Component } from "react";
+import axios from "axios"
+
+class MovieCard extends Component{
+    state = {
+        movieData: {}
+    };
+
+    componentDidMount() {
+        axios.get(`http://www.omdbapi.com/?i=${this.props.movieID}&apikey=8a9660ef`)
+            .then(res => res.data)
+            .then(res => {
+            this.setState({movieData: res})
+        })
+    }
+
+    render() {
+        const {
+            Title,
+            Released,
+            Genre,
+            Plot,
+            Poster,
+            imdbRating
+        } = this.state.movieData;
+        return (
+            <div className="movie-card-container">
+                <div className="image-container">
+                    <div className="bg-image" style={{ backgroundImage: `url(${Poster})` }}>
+                    </div>
+                </div>
+                <div className="movie-info">
+                    <h2>Movie Details</h2>
+                    <div>
+                        <h1>{Title}</h1>
+                        <small>Released Date: {Released}</small>
+                    </div>
+                </div>
+                <h4>Rating: {imdbRating} / 10</h4>
+                <p>{Plot && Plot.substr(0, 350)}</p>
+            </div>
+        )    
+    }
+}
+
+export default MovieCard
---
+++ src/components/MovieList/MovieList.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Component } from "react";
+import axios from "axios"
+import MovieCard from "../MovieCard/MovieCard";
+
+class MovieList extends Component{
+    state = {
+        movieList: [],
+        searchTerm: ''
+    }
+
+    search = event => {
+        event.preventDefault();
+        axios.get(`http://www.omdbapi.com/?apikey=8a9660ef&s=${this.state.searchTerm}&plot=full`)
+            .then(res => res.data)
+            .then(res => {
+                const movieList = res.Search.map(movie => movie.imdbID);
+                this.setState({ movieList })
+            })
+    }
+
+    handleChange = event => {
+        this.setState({ searchTerm: event.target.value });
+    }
+
+    render() {
+        const { movieList } = this.state;
+        return (
+            <div>
+                <form onSubmit={this.search}>
+                    <input placeholder='Search for a movie' onChange={this.handleChange}></input>
+                    <button type='submit'>Buscar</button>
+                </form>
+                {
+                    movieList.length > 0 ? (
+                        movieList.map(movie => (
+                            <MovieCard movieID={movie} key={movie}></MovieCard>
+                        ))
+                    ) : (
+                        <p>Não encontrou dados</p>
+                    )
+                }
+            </div>
+        )
+    }
+}
+
+export default MovieList;
